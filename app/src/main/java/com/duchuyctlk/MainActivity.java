4d71560360d6053a6debcd08db083fc64568c898
huy.duc <huy.duc@eastagile.com>
2016-04-13 12:06:01 +0700

    [f] Use ButterKnife on the demo app

--- app/src/main/java/com/duchuyctlk/MainActivity.java
+++ app/src/main/java/com/duchuyctlk/MainActivity.java
@@ -10,9 +10,12 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.view.Menu;
 import android.view.WindowManager;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 
 public class MainActivity extends Activity implements PopupDismissListener {
 
@@ -22,6 +25,15 @@ public class MainActivity extends Activity implements PopupDismissListener {
     private PopupDismissCatchableSpinner mSpinnerDialog;
     private int mDismissCount;
     private int mOpenCount;
+
+    @Bind(R.id.btn_use_listener)
+    ToggleButton btnUseListener;
+
+    @Override
+    protected void onDestroy() {
+        ButterKnife.unbind(this);
+        super.onDestroy();
+    }
 
     @SuppressWarnings("deprecation")
     @Override
@@ -42,24 +54,24 @@ public class MainActivity extends Activity implements PopupDismissListener {
         mSpinnerDropdown = (PopupDismissCatchableSpinner) findViewById(R.id.spinner_dropdown);
         mSpinnerDialog = (PopupDismissCatchableSpinner) findViewById(R.id.spinner_dialog);
 
-        ToggleButton btnUseListener = (ToggleButton) findViewById(R.id.btn_use_listener);
+        btnUseListener = (ToggleButton) findViewById(R.id.btn_use_listener);
         if (btnUseListener.isChecked()) {
             addListeners();
         }
 
-        btnUseListener.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    addListeners();
-                } else {
-                    removeListeners();
-                }
-            }
-        });
-
         mDismissCount = 0;
         mOpenCount = 0;
+
+        ButterKnife.bind(this);
+    }
+
+    @OnCheckedChanged(value = {R.id.btn_use_listener})
+    void onCheckedChanged(boolean isChecked) {
+        if (isChecked) {
+            addListeners();
+        } else {
+            removeListeners();
+        }
     }
 
     @Override
