b42ba6dcd59050b6dd2b3dfc89b353803258c598
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2020-09-02 15:06:58 +0700

    [c] Migrate to AndroidX

--- app/src/androidTest/java/com/duchuyctlk/integrations/MainActivityTest.java
+++ app/src/androidTest/java/com/duchuyctlk/integrations/MainActivityTest.java
@@ -1,8 +1,8 @@
 package com.duchuyctlk.integrations;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.espresso.Espresso;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import android.widget.Spinner;
 
 import com.duchuyctlk.MainActivity;
@@ -14,12 +14,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.spy;
 
